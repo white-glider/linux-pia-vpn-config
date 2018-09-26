e4094a731ffe5c8d642f085aca040151b2d983d5
Felix Almeida <white-glider@techwords.io>
2018-09-26 00:02:04 -0400

    Changed comp-lzo=no due to VORACLE attack. Added Apache 2 license header.

--- pia.sh
+++ pia.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#   Copyright 2016 Felix Almeida (white-glider)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
  
 # Get username
 echo -n "Enter your PIA VPN username: "
@@ -32,7 +45,7 @@ autoconnect=false
 service-type=org.freedesktop.NetworkManager.openvpn
 username=$PIAUSR
 dev=tun
-comp-lzo=yes
+comp-lzo=no
 remote=`echo $server | cut -d: -f2`
 port=1198
 cipher=AES-128-CBC
